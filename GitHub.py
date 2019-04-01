31fa802b1e6ba77f2dbc7db5829c4e8697196dee
Igor Postnikov <igor@shiftleft.io>
2019-04-01 09:21:57 -0700

    Minor changes in github.py

--- GitHub.py
+++ GitHub.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import pprint
 
 # GET /users/:username
 
@@ -10,4 +12,16 @@ print(r.reason)
 print(r.url)
 print(r.content)
 print(r.text)
-print(r.json())
+
+pprint.pprint(r.json())
+r = r.json()
+print(r["created_at"])
+print(r["location"])
+# json
+''' 
+
+json.dumps(a) - returns a string representing a json object from an object.
+json.loads(‘{“page”: 2, ...}’) - returns an object from a string representing a json object.
+
+
+'''
