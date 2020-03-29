0abb40c98bccf1b526e58cb5506e1f0c039d2782
Igor Postnikov <igor.postnikov8@gmail.com>
2020-03-28 20:47:12 -0700

    added youtube

--- Google_Maps.py
+++ Google_Maps.py
@@ -1,29 +1,21 @@
 import urllib.request
-import requests
 import json
 
-# endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-# api_key = 'AIzaSyCbIPJbqmg2QtYHJko5Ej69QYxO5_PhXpY'
-#
-# # Asks the user to input Where they are and where they want to go.
-# origin = input('Where are you?: ').replace(' ', '+')
-# destination = input('Where do you want to go?: ').replace(' ', '+')
-#
-# # Building the URL for the request
-# nav_request = 'origin={}&destination={}&key={}'.format(origin, destination, api_key)
-# request = endpoint + nav_request
-#
-# # Sends the request and reads the response.
-# response = urllib.request.urlopen(request).read()
-#
-# # Loads response as JSON
-# directions = json.loads(response)
-#
-# print(directions)
+endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+api_key = 'AIzaSyDquN6dX1DVZlPfig2uI9U_B7LW9QCEcwk'
 
-r = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCbIPJbqmg2QtYHJko5Ej69QYxO5_PhXpY"
+# Asks the user to input Where they are and where they want to go.
+origin = input('Where are you?: ').replace(' ', '+')
+destination = input('Where do you want to go?: ').replace(' ', '+')
 
-response = urllib.request.urlopen(r).read()
-#
+# Building the URL for the request
+nav_request = 'origin={}&destination={}&key={}'.format(origin, destination, api_key)
+request = endpoint + nav_request
+
+# Sends the request and reads the response.
+response = urllib.request.urlopen(request).read()
+
+# Loads response as JSON
 directions = json.loads(response)
+
 print(directions)
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
+## GitHub
+
 ## parametrs.py
 https://github.com/joeyajames/Python/blob/master/Web%20Data%20Mining/Python%20Requests.ipynb
 
-## google - direction api
+## google - directions api
 
 https://developers.google.com/maps/documentation/directions/get-api-key
 
 error: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+
+## youtube - data api
+
+https://www.youtube.com/watch?v=ZkYOvViSx3E
+
+https://github.com/vprusso/youtube_tutorials/commit/ee5758d08e9214d7c9117eda7176d3be8dda8778
